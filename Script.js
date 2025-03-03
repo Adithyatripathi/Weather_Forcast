bd8f4c160bae04d10beeb9b24e629bcf42f64395
Adithyatripathi <adithyatripathi1279@gmail.com>
2025-03-03 22:37:48 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,33 @@
+
+const apiKey = 'Your PASs KEY';
+
+
+async function updateWeather(city) {
+    if (!city) return;
+    
+    const [lat, lon] = citylist[city];
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}&aqi=no`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        
+        document.getElementById('cityName').textContent = `${data.location.name}, ${data.location.country}`;
+        document.getElementById('latitude').textContent = data.location.lat;
+        document.getElementById('longitude').textContent = data.location.lon;
+        document.getElementById('Temperature').textContent = data.current.temp_c;
+        document.getElementById('Feels-like').textContent = data.current.feelslike_c;
+        document.getElementById('Condition').textContent = data.current.condition.text;
+        document.getElementById('Humidity').textContent = data.current.humidity;
+        document.getElementById('wind_speed').textContent = data.current.wind_kph;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+
+document.getElementById('cities').addEventListener('change', (e) => {
+    const selectedCity = e.target.value;
+    updateWeather(selectedCity);
+});
